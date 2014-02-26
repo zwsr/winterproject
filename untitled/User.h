a57405bbcd958ce766f86c87240e9f340b7886b0
zwsr <weizhanginsjtu@gmail.com>
2014-02-26 12:57:15 +0800

    444

---
+++ untitled/User.h
@@ -0,0 +1,18 @@
+#ifndef _USER_H_
+#define
+class User
+{
+private:
+//essential information
+    string name;
+    int year_birth;
+    int month_birth;
+    int day_birth;
+
+//extra information
+    string gender;
+    string hometown;
+    string phonenumber;
+public:
+
+#endif // USER_H
